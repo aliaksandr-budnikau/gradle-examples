cfd4d031e0cd6dd00fb24caf2e71f43793922cc8
Your Name <you@example.com>
2015-12-17 17:20:59 +0300

    Executable jar

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,8 @@ public class Main {
 	public static String doSomething() {                          
 		return "done";
 	}
+
+	public static void main(String[] args) {
+		System.out.print(doSomething());
+	}
 }
