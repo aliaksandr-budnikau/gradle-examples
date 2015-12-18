d3cb44bf7756f23b2c1352859560f4e0631ca655
Your Name <you@example.com>
2015-12-18 18:52:16 +0300

    Add dependency

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,24 @@
-public class Main {
-	public static String doSomething() {                          
-		return "done";
-	}
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-	public static void main(String[] args) {
-		System.out.print(doSomething());
-	}
+public class Main {
+    private String str;
+
+    public static String doSomething() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
+        Main main = (Main) context.getBean("main");
+        return main.getStr();
+    }
+
+    public static void main(String[] args) {
+        System.out.print(doSomething());
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
 }
